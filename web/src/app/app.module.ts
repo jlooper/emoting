8cb08c5842c5e8865e7fdb9a4a6eab90b863e0af
tzmanics <tzmanics@gmail.com>
2017-03-14 11:34:34 -0400

    add firebase credentials

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AngularFireModule } from 'angularfire2';
+
 import { AppComponent } from './app.component';
 import { EmotingCardComponent } from './emoting-card/emoting-card.component';
 import { ResultChartComponent } from './result-chart/result-chart.component';
 import { EmojiVotesComponent } from './emoji-votes/emoji-votes.component';
+
+export const firebaseConfig = {
+   apiKey: "AIzaSyCVe0RQwBocNDOpUfDQfZf6Kh1E0-zcZxI",
+   authDomain: "emoting-b4d9a.firebaseapp.com",
+   databaseURL: "https://emoting-b4d9a.firebaseio.com",
+   storageBucket: "emoting-b4d9a.appspot.com",
+   messagingSenderId: "229031146500"
+};
 
 @NgModule({
   declarations: [
@@ -18,7 +28,8 @@ import { EmojiVotesComponent } from './emoji-votes/emoji-votes.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
