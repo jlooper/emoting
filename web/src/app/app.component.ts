4fb9986a48a9084de6b77cef8e42e0d00c9ce53e
tzmanics <tzmanics@gmail.com>
2017-03-17 00:12:29 -0400

    fetch data from db and display

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  photos: FirebaseListObservable<any[]>;
+  constructor (af: AngularFire){
+    this.photos = af.database.list('/Photos');
+  }
 }
