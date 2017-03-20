6f283261189498194fd4a22c9a068990407bef5d
tzmanics <tzmanics@gmail.com>
2017-03-19 21:33:58 -0400

    import hammerjs bc Kendo told me to ¯\_(ツ)_/¯

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { ChartModule } from '@progress/kendo-angular-charts';
 import { AngularFireModule } from 'angularfire2';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 
