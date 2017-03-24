95e2f771f767154e3a373ef1d06ae1aa1d3e5396
TJ VanToll <tj.vantoll@gmail.com>
2017-03-24 13:14:16 -0400

    Make the voting buttons work

--- web/src/app/home/home.component.ts
+++ web/src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
+import { Photo } from './photo.model';
+
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
@@ -8,12 +10,22 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
 })
 export class HomeComponent {
   photos: FirebaseListObservable<any[]>;
-  constructor (af: AngularFire){
+
+  constructor (af: AngularFire) {
     this.photos = af.database.list('/Photos',{
       query:{
         orderByChild: "date"
       }
     });
   }
-}
 
+  vote(index, photo: Photo) {
+    var updateObject = (index === 1) ? { emoji1: photo.emoji1++ } :
+      (index === 2) ? { emoji2: photo.emoji2++ } :
+      (index === 3) ? { emoji3: photo.emoji3++ } :
+      (index === 4) ? { emoji4: photo.emoji4++ } :
+      (index === 5) ? { emoji5: photo.emoji5++ } : {};
+
+    this.photos.update(photo.$key, updateObject);
+  }
+}
